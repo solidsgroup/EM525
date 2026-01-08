8f7b4592090352009c8d7e2c9ba0dc1d9d4246e5
Brandon Runnels <brunnels@iastate.edu>
2026-01-08 13:53:37 -0600

    clean up

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,6 @@
 #include <iostream>
-#include "eigen3/Eigen/Core"
-#include "eigen3/Eigen/Dense"
-#include "Set/Set.H"
 #include "Element/CST.H"
 #include "Element/Test.H"
-
-namespace Set
-{
-using Scalar = double;            // Note that "Set::Scalar" is the same as double
-using Vector = Eigen::Vector2d;   // Note that "Set::Vector" is the same as Eigen::Vector2d
-using Matrix = Eigen::Matrix2d;   // Note that "Set::Matrix" is the same as Eigen::Matrix2d
-}
 
 int main(int argc, char **argv)
 {
@@ -20,10 +10,17 @@ int main(int argc, char **argv)
     // using the tests defined in
     //    src/Element/Test.H
 
-    std::cout << "test.element.cst..." << std::endl;
-    Element::Test<Element::CST>::Dirac();
-    Element::Test<Element::CST>::SumToUnity();
-    Element::Test<Element::CST>::PhiDerivative();
-    std::cout << "                  ...tests pass!" << std::endl;
+    std::cout << "test.element.cst.diractest...";
+    try {Element::Test<Element::CST>::Dirac(); std::cout <<"pass"<<std::endl;}
+    catch(Util::Exception::UnitTest &e) {std::cout << "failed:" << std::endl << e.what() << std::endl;}
+
+    std::cout << "test.element.cst.sumtounitytest...";
+    try {Element::Test<Element::CST>::SumToUnity(); std::cout <<"pass"<<std::endl;}
+    catch(Util::Exception::UnitTest &e) {std::cout << "failed:" << std::endl << e.what() << std::endl;}
+
+    std::cout << "test.element.cst.phiderivativetest...";
+    try {Element::Test<Element::CST>::PhiDerivative(); std::cout <<"pass"<<std::endl;}
+    catch(Util::Exception::UnitTest &e) {std::cout << "failed:" << std::endl << e.what() << std::endl;}
+
 
 }
